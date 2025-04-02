61e2dec1d4722acd363de4328c3961d4f7629ed4
Kashik Sredharan <kashik.sredharan@gmail.com>
2025-04-02 23:48:29 +0530

    main

--- src/app/Dashboard.jsx
+++ src/app/Dashboard.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+
 import './globals.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function Dashboard() {
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+    return (
+        <>
+            <div className='main-container col'>
+                <div className='header'>
+                </div>
+                <div className='content row'>
+                    <div className='sidebar col'>
+                        <div className='uploading-area row'>
+                            <div className='main-upload'></div>
+                            <div className='image'></div>
+                        </div>
+                        <div className='pdf-preview col'>
+                            <div className='pdf-preview'></div>
+                            <div className='pdf-preview'></div>
+                        </div>
+                    </div>
+
+
+
+                    <div className='pdf-view'></div>
+
+                    <div className='text-area'></div>
+
+                </div>
+            </div>
+        </>
+    )
 }
 
-export default App
+export default Dashboard
